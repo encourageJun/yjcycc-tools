c67ac088e0b2cf7747c851382a242eed7ee50a52
yangjun <yangjun@xingxingcar.com>
2018-03-29 16:27:51 +0800

    新增: MyBatisBaseMapper

---
+++ yjcycc-tools/yjcycc-tools-common/src/main/java/org/yjcycc/tools/common/mapper/MyBatisBaseMapper.java
@@ -0,0 +1,25 @@
+package org.yjcycc.tools.common.mapper;
+
+import java.util.List;
+
+
+/**
+ * 基本的数据库CURD支持
+ * @author 许俊雄
+ * @since 2017-04-13
+ */
+public interface MyBatisBaseMapper<T> {
+
+	public T get(Integer id);
+	
+	public T get(T entity);
+	
+	public List<T> findPager(T entity);
+	
+	public int insert(T entity);
+	
+	public int update(T entity);
+	
+	public int delete(T entity);
+	
+}
